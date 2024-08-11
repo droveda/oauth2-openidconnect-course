c93ae3c48c888139731debe4d8a4c239fd4ba9cf
dieguesroveda <dieguesroveda@gmail.com>
2024-08-11 15:46:24 -0300

    added content

--- NOTES07.md
+++ NOTES07.md
@@ -30,3 +30,69 @@
 
 ## OAuth 2.0 - User Association to scopes
 
+## RBAC - Role based Access Control
+OAuth 2.0 - User association to Groups  
+
+![JWT](/images/rbac-01.png) 
+
+* Steps:
+  * Create the Application Groups
+  * Assign groups to Application Users
+  * Add "groups" claim to Access Token
+  * Modify Application to look at **Groups** instead of **Scopes**
+
+
+## OAuth 2.0 in the Cloud
+
+![JWT](/images/cloud-01.png) 
+
+![JWT](/images/cloud-02.png) 
+
+![JWT](/images/cloud-03.png) 
+
+
+## OAuth 2.0 and OpenID Connect
+* OAuth 2.0 is for Authorization
+  * Access token shloud contain only authorization information
+  * Scopres, Roles, Grant Types, Flows, Tokens
+  * No ID Token
+  * https://tools.ietf.org/html/rfc6749
+* OpenID Connect is the Identity layer on top of OAuth 2.0
+  * ID Token contains user claims
+  * /userinfo endpoint
+  * Scopres : openid profile email
+  * Can create custom claims
+  * Adds more Response types
+  * https://openid.net/specs/openid-connect-core-1_0.html
+
+
+### Some examples of when using OAuth 2.0 and OpenId Connect
+* Using booth OAuth 2.0 and OpenIdConnect
+  * Our my-photos google albums (Used to get user info like name, email, etc.. and used oauth to call the resource server to get the albums and the photos)
+* Only Using OpenIdConnect: (This apps gives us an option to log as...)
+  * Spofity (Continue with facebook, apple, google)
+  * Deezer
+  * Udemy
+* Only using OAuth 2.0
+  * When we are not interested in knowing the user information, but interested in calling a resource API
+  * Example a cronjob (client_credentials)
+  * Shutterfly application
+
+## Identity Brokers
+![JWT](/images/identity-broker.png)
+![JWT](/images/identity-broker-01.png) 
+![JWT](/images/identity-broker-02.png) 
+![JWT](/images/identity-broker-03.png) 
+
+## OAuth 2.0 Best Practices
+* Prefer Authorization Code Grant with PKCE
+* Prefer Client Credentials Grant for Cron Jobs
+* Avoid using Implicit Grant
+* Avoid using Resource Owner Password Grant
+* Store the secrets in a Safe place
+* Rotate the secrets regularly
+* Keep Access tokens short (5 Minutes)
+* Avoid using local users of the Authorization Server
+* Do not associate users with more scopes than needed
+* Use the enterprise logout (all sessions) You should also revoke the tokens
+* Do not store tokens or secrets in the browser or Mobile devices
--- okta-fakebookapi/src/main/java/com/mudra/fakebookapi/OAuth2SecurityConfig.java
+++ okta-fakebookapi/src/main/java/com/mudra/fakebookapi/OAuth2SecurityConfig.java
@@ -6,31 +6,46 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
 import org.springframework.security.web.SecurityFilterChain;
 
 /*
  * Security settings
  * 	- HTTP GET calls to /books/** requires fakebookapi.read scope
  * 	- HTTP POST call to /books require fakebookapi.admin scope
- * 	- tokens come in as JWT 
+ * 	- tokens come in as JWT
  * 	- No sessions created during requests (No JSESSIONID Cookie created)
- * 
+ *
  * CHANGE : With Spring Boot 3.0, we no longer need to extend WebSecurityConfigurerAdapter
  */
 @Configuration
 public class OAuth2SecurityConfig {
 
-	// CHANGE : With Spring Boot 3.0, Create a SecurityFilterChain 
-	@Bean
+    // CHANGE : With Spring Boot 3.0, Create a SecurityFilterChain
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-          .authorizeHttpRequests(authz -> authz
-            .requestMatchers(HttpMethod.GET, "/books/**").hasAuthority("SCOPE_fakebookapi.read")
-            .requestMatchers(HttpMethod.POST, "/books").hasAuthority("SCOPE_fakebookapi.admin")
-            .anyRequest().authenticated())
-          .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
-          .sessionManagement(sMgmt -> sMgmt.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers(HttpMethod.GET, "/books/**").hasAuthority("SCOPE_fakebookapi.read")
+                        .requestMatchers(HttpMethod.POST, "/books").hasAuthority("SCOPE_fakebookapi.admin")
+//                        .requestMatchers(HttpMethod.POST, "/books").hasAuthority("ROLE_book.admin")
+                        .anyRequest().authenticated())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .sessionManagement(sMgmt -> sMgmt.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
         return http.build();
-	}
+    }
+
+//    @Bean
+//    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+//        var jwtGrantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+//        jwtGrantedAuthoritiesConverter.setAuthoritiesClaimName("groups");
+//        jwtGrantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+//
+//        var jwtAuthConverter = new JwtAuthenticationConverter();
+//        jwtAuthConverter.setJwtGrantedAuthoritiesConverter(jwtGrantedAuthoritiesConverter);
+//        return jwtAuthConverter;
+//    }
+
 }
